561cb12b1c5ba3fffc9f4921a4164775853aaeae
besttiffe123 <pitthawat99chankaew@gmail.com>
2026-09-10 13:57:51 +0700

    New html

--- Javascript.js
+++ Javascript.js
@@ -1,5 +1,5 @@
 console.log("Hello javascript");
 console.log("hello Comsci <3");
-document.getElementsById("text").innerHTML = "This is first time for Javascript UwU";
+document.getElementById("text").innerHTML = "This is first time for Javascript UwU";
 document.write("Hello document");
 window.alert("welcome to My page ^-^")
